00d1c0a5186e7ce231bf418a03ee1d88f7cdefd0
J00n4 <joona.ylijoki@gmail.com>
2021-09-30 19:09:28 +0300

    trying to commit

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 
 
+
 const express = require("express");
 const os = require("os");
 const path = require("path");
@@ -11,13 +12,37 @@ app.use(express.json());
 
 
 
-app.use(express.static(path.join(__dirname, "public")));
+//app.use(express.static(path.join(__dirname, "public")));
+
+let recipes = [
+    {recipe: "pizza instructions ingredients"},
+    {recipe: "pasta instructions ingredients"},
+    {recipe: "potatoes instructions ingredients"}
+];
+
+app.get("/recipe/:food", (req, res) => {
+    /*res.json(recipes[req.params.food]);
+    const meal = req.query.food;
+    const instructions = {};
+    const ingredients = {};
+    res.send({
+        'Food': meal,
+        'Instructions': instructions,
+        'Ingredients': ingredients
+    });*/
+    const name = req.params.food;
+    const instructions = {};
+    const ingredients = {};
+    res.json({name});
+    //res.send(req.params);
+});
+
 
 app.get("/", (req, res) => {
     res.send("<h1>Hello World!</h1>");
 });
 
-app.use("/api/poems", require("./api/poems.js"));
+//app.use("/api/poems", require("./api/poems.js"));
 
 
 app.listen(port, () => console.log(`Server listening a port ${port}!`));
